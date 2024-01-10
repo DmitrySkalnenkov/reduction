49ce9ce38fa553209aeb46b9e99a8c96eaee3831
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2024-01-10 22:28:49 +0300

    + handlers functions

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/DmitrySkalnenkov/reduction/internal/app"
+	//	"github.com/DmitrySkalnenkov/reduction/internal/app"
 )
+
+//(i1)Сервер должен предоставлять два эндпоинта: POST / и GET /{id}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) { //(i1) Эндпоинт GET /{id} принимает в качестве URL-параметра идентификатор сокращённого URL и возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
+	if r.Method == http.MethodGet {
+
+	} else {
+		return
+	}
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) { //(i1) Эндпоинт POST / принимает в теле запроса строку URL для сокращения и возвращает ответ с кодом 201 и сокращённым URL в виде текстовой строки в теле.
+	if r.Method == http.MethodPost {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		resp, err := json.Marshal(b)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(resp)
+	}
+}
 
 func main() {
 	var hostPortStr string = "localhost:8080" //(i1) Сервер должен быть доступен по адресу: http://localhost:8080.
@@ -16,6 +45,7 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  30 * time.Second,
 	}
+	http.HandleFunc("/", PostHandler)
 
 	log.Fatal(s.ListenAndServe())
 }
