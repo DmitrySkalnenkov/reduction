507659b5f1ed39b571e3c1c37c19eb81a0491fbf
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2024-01-10 21:52:34 +0300

    + first commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/DmitrySkalnenkov/reduction/internal/app"
+)
+
+func main() {
+	var hostPortStr string = "localhost:8080" //(i1) Сервер должен быть доступен по адресу: http://localhost:8080.
+	s := &http.Server{
+		Addr:         hostPortStr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
