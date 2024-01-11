81e3569a0bdcceb6f0d713ab7ab38c2988099290
dmitryskalnenkov <dmitry.skalnenkov@gs-labs.ru>
2024-01-11 21:13:32 +0300

    added unit tests for randomString()

---
+++ internal/app/app_test.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	//	"net/http/httptest"
+	"fmt"
+	"testing"
+)
+
+func TestRandomString(t *testing.T) {
+	tests := []struct {
+		name     string
+		inputVal int
+		wantVal  int
+	}{ //Test table
+		{
+			name:     "Positive test. Lenght 10",
+			inputVal: 10,
+			wantVal:  10,
+		},
+		{
+			name:     "Positive test. Lenght 2",
+			inputVal: 2,
+			wantVal:  2,
+		},
+	}
+	var resultStr string
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			resultStr = randomString(tt.inputVal)
+			fmt.Printf("RandomString(%d) result is: '%s'\n", tt.inputVal, resultStr)
+			if len(resultStr) != tt.wantVal {
+				t.Errorf("TEST_ERROR: input value is %d,  want is %d ", tt.inputVal, tt.wantVal)
+			}
+		})
+	}
+}
